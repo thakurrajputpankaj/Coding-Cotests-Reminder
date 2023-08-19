68fec74500f8c312399e93dbedbcdd849d22c9a5
PANKAJ RAJPUT <thakurrajputpankaj0@gmail.com>
2023-08-18 20:07:30 -0400

    added buttons

--- app/src/main/java/com/pankaj/contesttime/MainActivity.java
+++ app/src/main/java/com/pankaj/contesttime/MainActivity.java
@@ -3,6 +3,8 @@ package com.pankaj.contesttime;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
@@ -16,18 +18,67 @@ import retrofit2.Response;
 public class MainActivity extends AppCompatActivity {
     private ApiService apiService;
     TextView textView;
+    Button all;
+    Button hacker_rank;
+    Button top_coder;
+    Button code_chef;
+    Button code_forces;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         textView = (TextView) findViewById(R.id.data);
+        all = (Button) findViewById(R.id.all);
+        hacker_rank = (Button) findViewById(R.id.hacker_rank);
+        top_coder = (Button) findViewById(R.id.top_coder);
+        code_chef = (Button) findViewById(R.id.code_chef);
+        code_forces = (Button) findViewById(R.id.code_forces);
 
         //textView.setText("Hello");
 
         apiService = ApiClient.getClient("https://kontests.net/api/v1/").create(ApiService.class);
-        Call<List<Post>> call = apiService.getPost();
-        EnqueueCall(call);
+
+        all.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<List<Post>> call = apiService.getPost();
+                EnqueueCall(call);
+            }
+        });
+
+        hacker_rank.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<List<Post>> call = apiService.getHackerRank();
+                EnqueueCall(call);
+            }
+        });
+
+        top_coder.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<List<Post>> call = apiService.getTopCoder();
+                EnqueueCall(call);
+            }
+        });
+
+        code_forces.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<List<Post>> call = apiService.getCodeForces();
+                EnqueueCall(call);
+            }
+        });
+
+        code_chef.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Call<List<Post>> call = apiService.getCodechef();
+                EnqueueCall(call);
+            }
+        });
+
     }
 
     public void EnqueueCall (Call<List<Post>> call){
